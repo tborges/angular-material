20790f5a75d20e246085c1469fd2518199e919fd
tborges <tborgesmail@yahoo.com>
2018-07-12 20:32:57 -0700

    animations
    animations

--- ng5-material/src/app/app.module.ts
+++ ng5-material/src/app/app.module.ts
@@ -3,8 +3,8 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {MaterialModule} from './material.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './material.module';
 
 @NgModule({
   declarations: [
